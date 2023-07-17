14bf427922e78fd76229789552e888188223619f
Juned Khan <developmentbyjuned@gmail.com>
2023-07-17 23:05:54 +0530

    feat: Add accordion container blueprint.
    - Added accordion container blueprint.

---
+++ app/containers/AccordionContainer/AccordionContainer.tsx
@@ -0,0 +1,13 @@
+/**
+ * AccordionContainer component represents a container for an accordion section.
+ *
+ * This component renders the container for an accordion section, which can be
+ * used to display collapsible content with headings and expandable panels.
+ *
+ * @returns {JSX.Element} The JSX element representing the AccordionContainer component.
+ */
+export default function AccordionContainer() {
+  return (
+    <div>AccordionContainer</div>
+  )
+}
