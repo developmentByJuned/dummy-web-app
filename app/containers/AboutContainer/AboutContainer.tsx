f9eed69e0a98b67c886e98f48ce9911840abdc58
Juned Khan <developmentbyjuned@gmail.com>
2023-07-17 23:05:22 +0530

    feat: Add about container blueprint.
    - Added about container blueprint.

---
+++ app/containers/AboutContainer/AboutContainer.tsx
@@ -0,0 +1,13 @@
+/**
+ * AboutContainer component represents a container for the about section.
+ *
+ * This component renders the container for the about section, which can be used
+ * to display information about the website, company, or application.
+ *
+ * @returns {JSX.Element} The JSX element representing the AboutContainer component.
+ */
+export default function AboutContainer() {
+  return (
+    <div>AboutContainer</div>
+  )
+}
